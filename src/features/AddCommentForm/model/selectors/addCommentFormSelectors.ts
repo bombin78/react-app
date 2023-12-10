78522ea00e94f418d572738912841f54f5948fc6
bombin78 <bombin78@mail.ru>
2023-12-10 11:23:35 +0600

    fix "getAddCommentFormText" selector

--- src/features/AddCommentForm/model/selectors/addCommentFormSelectors.ts
+++ src/features/AddCommentForm/model/selectors/addCommentFormSelectors.ts
@@ -1,5 +1,7 @@
 import { StateSchema } from 'app/providers/StoreProvider';
 
-export const getAddCommentFormText = (state: StateSchema) => state.addCommentForm?.text;
+// Используем оператор нулевого слияния (nullish coalescing operator) вместо оператора "или",
+// чтобы при вводе в input значения "0" оно не было преобразовано в "false"
+export const getAddCommentFormText = (state: StateSchema) => state.addCommentForm?.text ?? '';
 
 export const getAddCommentFormError = (state: StateSchema) => state.addCommentForm?.error;
