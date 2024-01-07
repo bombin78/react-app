b8f00ceb878deb787825ce6bd8a1458457830e46
bombin78 <bombin78@mail.ru>
2024-01-07 23:00:45 +0600

    9.62.14 fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,13 @@ import { LOCAL_STORAGE_THEME_KEY } from '../../app/providers/ThemeProvider/lib/T
 // Создаем инстанс axios
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || '',
-    },
+});
+
+// C помощью interceptors, который будет отрабатывать перед запросом,
+// добавляем поле authorization, чтобы оно не было пустым в запросе
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.authorization = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || '';
+    }
+    return config;
 });
